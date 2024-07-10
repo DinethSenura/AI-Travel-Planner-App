ec6a7b7f1f5f227d248456704827aed0aadee346
Dineth Senura <100292025+DinethSenura@users.noreply.github.com>
2024-07-10 20:19:26 +0530

    update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,7 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import {CreateTripContext} from '../context/CreateTripContext'
+import { useState } from "react";
 
 export default function RootLayout() {
 
@@ -8,7 +10,10 @@ export default function RootLayout() {
     'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
     'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
   })
+
+  const [tripData,setTripData]=useState([]);
   return (
+    <CreateTripContext.Provider value={{tripData,setTripData}}>
     <Stack screenOptions={{
       headerShown:false
     }}>
@@ -17,5 +22,6 @@ export default function RootLayout() {
       }} /> */}
       <Stack.Screen name="(tabs)"/>
     </Stack>
+    </CreateTripContext.Provider>
   );
 }
---
+++ constants/Options.js
@@ -0,0 +1,33 @@
+export const SelectTravelesList=[
+{
+    title:'Just Me',
+    desc:'A sole traveles in exploration',
+    icon:'🛩',
+    people:'1'
+
+},
+{
+    id:2,
+    title:'A Couple',
+    desc:'Two traveles in tandem',
+    icon:'🥂',
+    people:'2 People'
+
+},
+{
+    id:3,
+    title:'Family',
+    desc:'A group of fun loving adv',
+    icon:'🏡',
+    people:'3 to 5 People'
+
+},
+{
+    id:4,
+    title:'Friends',
+    desc:'A bunch of thrill-seekes',
+    icon:'⛵',
+    people:'5 to 10 People'
+
+}
+]
