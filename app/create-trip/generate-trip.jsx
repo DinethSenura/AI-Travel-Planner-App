1f2b05f36215ca252e6a372ebf6157e45afd84cb
Dineth Senura <100292025+DinethSenura@users.noreply.github.com>
2024-07-12 18:40:33 +0530

    update

--- app/create-trip/generate-trip.jsx
+++ app/create-trip/generate-trip.jsx
@@ -1,10 +1,32 @@
-import { View, Text, Image } from 'react-native'
-import React, {useContext} from 'react'
+import { View, Text, Image, StyleSheet } from 'react-native'
+import React, {useContext, useEffect} from 'react'
 import { Colors } from '../../constants/Colors'
 import { CreateTripContext } from '../../context/CreateTripContext'
+import { AI_PROMPT } from '../../constants/Options';
+
 
 export default function GenerateTrip() {
-  const {tripData,setTripData}=useContext(CreateTripContext)
+  const {tripData,setTripData}=useContext(CreateTripContext);
+
+  useEffect(()=>{
+    tripData&&GenerateAiTrip()
+  },[tripData])
+
+  const GenerateAiTrip=()=>{
+    const FINAL_PROMPT=AI_PROMPT
+    .replace('{location}',tripData?.locationInfo?.name)
+    .replace('{totalDays}',tripData.totalNoOfDays)
+    .replace('{totalNight}',tripData.totalNoOfDays-1)
+    .replace('{traveler}',tripData.traveler?.title)
+    .replace('{budget}',tripData.budget)
+    .replace('{totalDays}',tripData.totalNoOfDays)
+    .replace('{totalNight}',tripData.totalNoOfDays-1);
+    
+    console.log(FINAL_PROMPT);
+    ;
+
+  }
+    
   return (
     <View style={{
       padding:25,
