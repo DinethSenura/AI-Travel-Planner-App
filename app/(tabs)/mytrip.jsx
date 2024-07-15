0bcda220a44c31e011be2f75ce96f22cc9a0e684
Dineth Senura <100292025+DinethSenura@users.noreply.github.com>
2024-07-15 14:31:47 +0530

    update

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,12 +1,31 @@
 import { View, Text } from 'react-native'
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import { Colors } from './../../constants/Colors'
 import { Ionicons } from '@expo/vector-icons';
 import StartNewTripCard from '../../components/MyTrips/StartNewTripCard';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import {auth,db} from './../../configs/FirebaseConfig'
 
 export default function MYTrip() {
 
   const [userTrips,setUserTrips]=useState([]);
+  const user=auth.currentUser;
+
+  useEffect(()=>{
+    user&&GetMyTrips();
+  },[user])
+
+  const GetMyTrips=async()=>{
+    const q=query(collection(db,'UserTrip'),where('userEmail','==',user?.email));
+    const querySnapshot=await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+
+
+  }
   return (
     <View style={{
       padding:25,
